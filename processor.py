7e7cc16cef31968d5b274b8cfec4f367e0461d58
Toby <tohongwong@gmail.com>
2024-05-08 15:57:41 +0800

    use relative path & remove file after process

--- processor.py
+++ processor.py
@@ -1,4 +1,4 @@
-from io import BytesIO
+import os
 import threading
 import time
 import json
@@ -28,6 +28,12 @@ queue = Redis(host="default-redis-0001-001.7zh2ph.0001.use1.cache.amazonaws.com"
 session = boto3.Session(region_name="us-east-1")
 s3 = session.client("s3")
 
+def remove_file(filename):
+    try:
+        os.remove(filename)
+    except Exception as e:
+        print(f"Failed to remove file: {e}")
+
 def processQueue():
     while True:
         try:
@@ -36,10 +42,11 @@ def processQueue():
             task_id = task["id"]
             text_to_speak = task["text"]
             print(f"Processing Task: id={task_id}, text={text_to_speak}")
-            s3.download_file("ai-presenter", f"tasks/{task_id}.wav", f"/tmp/{task_id}.wav")
-            reference_audios = [f"./tmp/{task_id}.wav"]
+            s3.download_file("ai-presenter", f"tasks/{task_id}.wav", f"tmp/{task_id}.wav")
+            reference_audios = [f"tmp/{task_id}.wav"]
         except Exception as e:
             print(f"Failed to retrieve data: {e}")
+            remove_file(f"tmp/{task_id}.wav")
             continue
 
         try:
@@ -52,13 +59,17 @@ def processQueue():
             )
         except Exception as e:
             print(f"Failed to process: {e}")
+            remove_file(f"tmp/{task_id}.wav")
             continue
             
         try:
             s3.upload_fileobj(outputs['wav'], "ai-presenter", f"results/{task_id}.wav", ExtraArgs={'ContentType': "audio/wav"})
         except Exception as e:
             print(f"Failed to upload result: {e}")
+            remove_file(f"tmp/{task_id}.wav")
             continue
+        
+        remove_file(f"tmp/{task_id}.wav")
 
 if __name__ == '__main__':
     print("Starting up...")
